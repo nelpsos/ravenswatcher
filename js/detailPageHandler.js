2fc9809b6eba615061b7ec50b6768e008477d0d1
라브소소 <nelpsos@gmail.com>
2025-02-26 15:51:54 +0900

    클릭해서 열 옮기는 기능 구현

--- js/detailPageHandler.js
+++ js/detailPageHandler.js
@@ -1,16 +1,35 @@
-const detailPage = document.getElementById("detail-page");
-
 detailPage.addEventListener("click", (event) => {
   const item = event.target.closest(".item");
   if (item) {
-    const row = item.parentElement.parentElement.dataset.row;
+    const itemBlock = item.parentElement;
+    const row = itemBlock.parentElement.parentElement.dataset.row;
+    const leftCol = detailPage.querySelector(`[data-row="${row}"] .left-col`);
     const rightCol = detailPage.querySelector(`[data-row="${row}"] .right-col`);
-    const placeholder = rightCol.querySelector(".placeholder");
-    if (placeholder) {
-      placeholder.replaceWith(item);
-      detailPage
-        .querySelector(`[data-row="${row}"] .left-col`)
-        .appendChild(placeholder);
+
+    if (itemBlock.parentElement.classList.contains("right-col")) {
+      // 오른쪽 열의 아이템 클릭 시 왼쪽 열로 이동
+      const originalItemBlock = leftCol.querySelector(
+        `[data-item-id="${item.dataset.itemId}"]`
+      );
+      if (originalItemBlock) {
+        const placeholder = originalItemBlock.querySelector(".placeholder");
+        if (placeholder) {
+          placeholder.replaceWith(item);
+          itemBlock.appendChild(placeholder);
+        } else {
+          originalItemBlock.appendChild(item);
+        }
+      }
+    } else {
+      const placeholder = rightCol.querySelector(".placeholder");
+      // 왼쪽 열의 아이템 클릭 시 오른쪽 열로 이동
+      if (placeholder) {
+        console.log(placeholder.parentElement);
+        console.log(item);
+        placeholder.parentElement.dataset.itemId = item.dataset.itemId;
+        placeholder.replaceWith(item);
+        itemBlock.appendChild(placeholder);
+      }
     }
   }
 });
